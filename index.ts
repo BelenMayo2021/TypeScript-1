e59af220299cfc8da1bb623267f8dbe926935d98
Belen <belen_m_17@hotmail.com>
2022-01-05 16:28:58 -0300

    commit-3

--- index.ts
+++ index.ts
@@ -9,3 +9,32 @@ let booleano: boolean = false;
 console.log(nombre);
 console.log(persona);
 console.log(booleano);
+
+
+//interfaces
+
+//Una pequeña configuracion de los objetos
+
+interface Personaje{
+    nombre: string;
+    pv: number;
+    habilidades: string [];
+    ciudad?: string //éste atributo es opcional
+
+}
+
+
+//Objeto plano
+
+const personaje: Personaje = {
+    nombre: 'Aquaman',
+    pv: 100,
+    habilidades: ['Nadar', 'Fuerza', 'Agilidad' ]
+}
+
+console.log(personaje);
+
+
+
+
+

9434f821f9565bf0be17054758ee865aa62e77d5
Belen <belen_m_17@hotmail.com>
2022-01-05 16:42:32 -0300

    commit-5

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 console.log("Me imprime en consola");
 //Variables
 var nombre = 'Santiago';
+var persona = 100;
+var booleano = false;
+console.log(nombre);
+console.log(persona);
+console.log(booleano);
+//Objeto plano
+var personaje = {
+    nombre: 'Aquaman',
+    pv: 100,
+    habilidades: ['Nadar', 'Fuerza', 'Agilidad']
+};
+console.log(personaje);

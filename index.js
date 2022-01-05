bfe25593badc6becaf03b82e6334750229bd20da
Belen <belen_m_17@hotmail.com>
2022-01-05 16:11:23 -0300

    commit 2

---
+++ index.js
@@ -0,0 +1,3 @@
+console.log("Me imprime en consola");
+//Variables
+var nombre = 'Santiago';
--- index.ts
+++ index.ts
@@ -2,4 +2,10 @@ console.log("Me imprime en consola");
 
 //Variables
 
-let nombre:string = 'Santiago'
+let nombre: string = 'Santiago'
+let persona: number = 100;
+let booleano: boolean = false;
+
+console.log(nombre);
+console.log(persona);
+console.log(booleano);
